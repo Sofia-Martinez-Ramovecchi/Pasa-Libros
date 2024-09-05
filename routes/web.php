28dc74d2130460c3b15d307c366b778dd1dbb0bd
Sofia-Martinez-Ramovecchi <somartinez@uade.edu.ar>
2024-09-05 11:32:28 -0300

    refactor servicechatprofanity and add controller and route

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,11 @@
 <?php
 
+use App\Http\Controllers\ControllerValidateMessage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Services\ServiceChatProfanity;
 //IDEA: mejorar el return de los metodos, ver como crear un controller y rutearlo, ya que no respeta principio de responsabilidad unica
-Route::get('post', function (Request $request, ServiceChatProfanity $service) {
-    $message = $request->input('message', '');  // Obtiene el mensaje desde los parámetros de la solicitud
-    $result = $service->isProfanity($message);
-
-    return response()->json(['isProfanity' => $result]);
-});
+Route::resource('message',ControllerValidateMessage::class);
 
 
 Route::get('/', function () {
