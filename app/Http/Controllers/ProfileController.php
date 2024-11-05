7469ff07becb6e9dc810552649f5855afa7eb13f
Sofia-Martinez-Ramovecchi <somartinez@uade.edu.ar>
2024-11-05 20:40:18 -0300

    modifico el controler del perfil

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -14,13 +14,17 @@ class ProfileController extends Controller
     /**
      * Display the user's profile form.
      */
-    public function edit(Request $request): View
+    /*public function edit(Request $request): View
     {
         return view('profile.edit', [
             'user' => $request->user(),
         ]);
+    }*/
+    public function mostrar(Request $request): View{
+        return view('Perfil',[
+            'user'=>$request->user(),
+        ]);
     }
-
     /**
      * Update the user's profile information.
      */
@@ -34,7 +38,7 @@ class ProfileController extends Controller
 
         $request->user()->save();
 
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+        return Redirect::route('perfil')->with('status', 'profile-updated');
     }
 
     /**
