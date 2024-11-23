685977910ecb1059d3306ffdf3d28f2dc51567c6
Leandro-Zalazar <111947228+Leandro-Zalazar@users.noreply.github.com>
2024-11-23 13:43:36 -0300

    laravel-scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,6 @@ class Kernel extends ConsoleKernel
     {
         // Aquí defines las tareas programadas
 
-        $schedule->command('usuarios:revisar-suspensiones')->daily();
+        $schedule->command('usuarios:revisar-suspensiones')->everyMinute();
     }
 }
--- laravel-scheduler.bat
+++ laravel-scheduler.bat
@@ -1,3 +1,6 @@
 @echo off
 cd C:\xampp\htdocs\Pasa-Libros
-php artisan schedule:run
+echo Ejecutando php artisan schedule:run...
+php artisan schedule:run >> scheduler.log 2>&1
+echo Comando ejecutado.
+pause
